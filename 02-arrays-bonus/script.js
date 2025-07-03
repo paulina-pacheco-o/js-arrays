dcf639e304d3f6940cfad3849c25e22c35e3dd65
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-03 17:33:50 +0200

    second exercise bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,7 +15,16 @@ const teachers = [
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = []
+for (let i = 0; i < teachers.length; i++) {
+  const thisName = teachers[i];
+  if (thisName.length >= 5) {
+    longNames.push(thisName)
+  }
+}
+
+console.log(teachers);
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 //teachers.splice(1, 1);
@@ -23,9 +32,9 @@ const longNames = null;
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = teachers.indexOf("Fabio");
-console.log(isFabioPresent);
+//const isFabioPresent = teachers.indexOf("Fabio");
+//console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = teachers.toString();
-console.log(teachersString)
+//const teachersString = teachers.toString();
+//console.log(teachersString)
