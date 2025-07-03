fa80b9dd3c31eaa42a5039a5b03c2668842f501f
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-03 16:31:53 +0200

    third exercise bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,14 +10,16 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
-console.log(reversedTeachers)
+//const reversedTeachers = teachers.reverse();
+//console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = null;
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(1, 1);
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
